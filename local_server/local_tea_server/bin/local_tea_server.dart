c2e8d7b740b823e436ef405fe2674cb225eaa83a
Matvey <matvey.works@gmail.com>
2025-05-21 10:41:25 +0400

    Changed dart version

--- local_server/local_tea_server/bin/local_tea_server.dart
+++ local_server/local_tea_server/bin/local_tea_server.dart
@@ -1,5 +1,3 @@
-import 'package:local_tea_server/local_tea_server.dart' as local_tea_server;
-
-void main(List<String> arguments) {
-  print('Hello world: ${local_tea_server.calculate()}!');
+void main() {
+  print("anyone");
 }
